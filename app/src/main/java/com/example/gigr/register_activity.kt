e763bbcda16a881a308bec175cec89064c28db9d
Ld. Nelson <67654295+MwangiNelson@users.noreply.github.com>
2022-04-26 01:12:26 +0300

    login & reg. form implemented

--- app/src/main/java/com/example/gigr/register_activity.kt
+++ app/src/main/java/com/example/gigr/register_activity.kt
@@ -5,18 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class login_activity : AppCompatActivity() {
+
+class register_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
 
+        var loginLink = findViewById<Button>(R.id.login_link)
 
-        var signUpLink = findViewById<Button>(R.id.sign_up_link)
-
-        signUpLink.setOnClickListener{
-            val intent = Intent(this@login_activity, register_activity::class.java)
+        loginLink.setOnClickListener{
+            val intent = Intent(this@register_activity, login_activity::class.java)
             startActivity(intent)
             finish()
         }
+
     }
 }
