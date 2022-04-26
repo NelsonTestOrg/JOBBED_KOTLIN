c21a1601410184843b7fe863892501be9e2a0ca0
Ld. Nelson <67654295+MwangiNelson@users.noreply.github.com>
2022-04-26 20:54:09 +0300

    login & registration working

--- app/src/main/java/com/example/gigr/login_activity.kt
+++ app/src/main/java/com/example/gigr/login_activity.kt
@@ -3,7 +3,11 @@ package com.example.gigr
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class login_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,11 +16,74 @@ class login_activity : AppCompatActivity() {
 
 
         var signUpLink = findViewById<Button>(R.id.sign_up_link)
+        var login_button = findViewById<Button>(R.id.login_button)
+        var userLoginEmail = findViewById<TextInputEditText>(R.id.login_email)
+        var userLoginPassword = findViewById<TextInputEditText>(R.id.login_password)
+
+
 
         signUpLink.setOnClickListener{
             val intent = Intent(this@login_activity, register_activity::class.java)
             startActivity(intent)
             finish()
+        }
+
+        login_button.setOnClickListener {
+            when {
+                TextUtils.isEmpty(userLoginEmail.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this,
+                        "Enter Email to log in",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                TextUtils.isEmpty(userLoginPassword.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this,
+                        "Enter a Password.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                else -> {
+                    val loginemail: String = userLoginEmail.text.toString().trim { it <= ' ' }
+                    val loginpassword: String = userLoginPassword.text.toString().trim { it <= ' ' }
+
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(loginemail, loginpassword)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+
+                                Toast.makeText(
+                                    this,
+                                    "Login successful.",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+
+                                val intent = Intent(this, MainActivity::class.java)
+
+                                intent.flags =
+                                    Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                intent.putExtra("user_email", loginemail)
+                                startActivity(intent)
+                                finish()
+
+
+                            } else {
+                                Toast.makeText(
+                                    this,
+                                    task.exception!!.message.toString(),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+
+                        }
+
+                }
+
+            }
+
+
         }
     }
 }
--- app/src/main/java/com/example/gigr/register_activity.kt
+++ app/src/main/java/com/example/gigr/register_activity.kt
@@ -3,7 +3,14 @@ package com.example.gigr
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 
 class register_activity : AppCompatActivity() {
@@ -12,11 +19,76 @@ class register_activity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
 
         var loginLink = findViewById<Button>(R.id.login_link)
+        var registerButton = findViewById<Button>(R.id.register_button)
+        var userEmail = findViewById<TextInputEditText>(R.id.register_email)
+        var userPassword = findViewById<TextInputEditText>(R.id.register_password)
+        var userConfirmPassword = findViewById<TextInputEditText>(R.id.confirm_register_password)
 
         loginLink.setOnClickListener{
             val intent = Intent(this@register_activity, login_activity::class.java)
             startActivity(intent)
             finish()
+        }
+
+        registerButton.setOnClickListener {
+
+            when {
+                TextUtils.isEmpty(userEmail.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this,
+                        "Enter Email",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                TextUtils.isEmpty(userPassword.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this,
+                        "Enter a Password.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+
+                else -> {
+                    val email: String = userEmail.text.toString().trim { it <= ' ' }
+                    val password: String = userPassword.text.toString().trim { it <= ' ' }
+
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(
+                            OnCompleteListener<AuthResult> { task ->
+                                if (task.isSuccessful) {
+
+                                    val firebaseUser: FirebaseUser = task.result!!.user!!
+                                    Toast.makeText(
+                                        this,
+                                        "Registration successful.",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+
+                                    val intent =
+                                        Intent(this, MainActivity::class.java)
+                                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                    intent.putExtra("user_id", firebaseUser.uid)
+                                    intent.putExtra("user_email", email)
+                                    startActivity(intent)
+                                    finish()
+
+
+                                } else {
+                                    Toast.makeText(
+                                        this,
+                                        task.exception!!.message.toString(),
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+
+                            }
+                        )
+
+                }
+
+            }
         }
 
     }
